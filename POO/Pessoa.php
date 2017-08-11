78c5ca5521c541a795e4eee0b8046de9ec4c8815
cesar paulo alves <caesaralves@gmail.com>
2017-08-11 18:46:43 +0100

    teste

--- POO/Pessoa.php
+++ POO/Pessoa.php
@@ -9,4 +9,19 @@
 class Pessoa
 {
 
+   public $nome;
+   public $idade;
+
 }
+
+$pessoa1 = new Pessoa();
+$pessoa2 = new  Pessoa();
+
+$pessoa1->nome = "Cesar";
+$pessoa1->idade = 25;
+
+$pessoa2->nome = "Maria";
+$pessoa2->idade = 10;
+
+echo "Pessoa1 = {$pessoa1->nome} {$pessoa1->idade}";
+echo "Pessoa2 = {$pessoa2->nome} {$pessoa2->idade}";
